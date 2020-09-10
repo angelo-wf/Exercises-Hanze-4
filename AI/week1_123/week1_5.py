c1cf65bd44a5b01fc2ab9cab09db80304fb4144b
elzo_d <32901471+elzo-d@users.noreply.github.com>
2020-09-10 10:14:32 +0200

    ex 5: works for N*N board, added heuristic

--- AI/week1_123/week1_5.py
+++ AI/week1_123/week1_5.py
@@ -1,6 +1,9 @@
 import math
 import random
 
+SIZE = 3 # board dimension (3 = 3x3 board)
+USE_HEURISTIC = True # use heuristic (False = UCS, True = A*)
+
 # state is a tuple of 9 items with empty spot as 0
 
 def get_next(cur):
@@ -13,28 +16,28 @@ def get_next(cur):
         i += 1
 
     final = []
-    if i < 6:
+    if i < (SIZE * (SIZE - 1)):
         # we can move up into the spot
         new = current.copy()
-        item = new[i + 3]
+        item = new[i + SIZE]
         new[i] = item
-        new[i + 3] = 0
+        new[i + SIZE] = 0
         final.append(tuple(new))
-    if i > 2:
+    if i > (SIZE - 1):
         # we can move down into the spot
         new = current.copy()
-        item = new[i - 3]
+        item = new[i - SIZE]
         new[i] = item
-        new[i - 3] = 0
+        new[i - SIZE] = 0
         final.append(tuple(new))
-    if (i % 3) < 2:
+    if (i % SIZE) < (SIZE - 1):
         # we can move left into the spot
         new = current.copy()
         item = new[i + 1]
         new[i] = item
         new[i + 1] = 0
         final.append(tuple(new))
-    if (i % 3) > 0:
+    if (i % SIZE) > 0:
         # we can move right into the spot
         new = current.copy()
         item = new[i - 1]
@@ -44,7 +47,33 @@ def get_next(cur):
     return final
 
 def heuristic(current, goal):
-    return 0
+    if not USE_HEURISTIC:
+        return 0
+
+    # heuristic: the sum of the distances between current pos and goal pos of each non-empty tile
+    totaldist = 0
+    for i in range(1, SIZE*SIZE):
+        # for each spot not empty
+        # figure out where they are in both boards
+        curloc = 0
+        for s in current:
+            if s == i:
+                break
+            curloc += 1
+        goalloc = 0
+        for s in goal:
+            if s == i:
+                break
+            goalloc += 1
+        # get them as coords
+        curx = curloc % SIZE
+        cury = curloc // SIZE
+        goalx = goalloc % SIZE
+        goaly = goalloc // SIZE
+        # get distance between them
+        dist = abs(curx - goalx) + abs(cury - goaly)
+        totaldist += dist
+    return totaldist
 
 def get_score(first, second):
     return 1
@@ -87,6 +116,13 @@ def a_star(start, goal):
                 if not neighbor in frontier:
                     frontier.add(neighbor)
 
+def create_board():
+    board = []
+    for i in range(SIZE*SIZE):
+        board.append(i + 1)
+    board[SIZE*SIZE - 1] = 0
+    return tuple(board)
+
 def randomized_board(start, count):
     state = start
     for i in range(count):
@@ -95,12 +131,14 @@ def randomized_board(start, count):
     return state
 
 def print_board(current):
-    a, b, c, d, e, f, g, h, i = current
-    print("{} {} {}\n{} {} {}\n{} {} {}".format(a, b, c, d, e, f, g, h, i))
+    for y in range(SIZE):
+        for x in range(SIZE):
+            print("{:02d} ".format(current[y * SIZE + x]), end="")
+        print("")
 
 
-goal = (1, 2, 3, 4, 5, 6, 7, 8, 0)
-start = randomized_board(goal, 40)
+goal = create_board()
+start = randomized_board(goal, 20)
 
 print("Starting board:")
 print_board(start)
